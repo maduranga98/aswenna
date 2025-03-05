a5ab9b8b249be02b07088b408291667198c7896d
maduranga98 <madurangalakbima@gmail.com>
2025-03-05 16:47:12 +0530

    auth correction and updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,6 +45,10 @@ android {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.2.2")
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
     implementation("com.google.firebase:firebase-analytics")
 }
 
